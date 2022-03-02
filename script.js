b0c8071d8dfb79a89dfecd035dcad230cc3f9878
tafaramafemba <tafara.maf@gmail.com>
2022-03-02 20:22:32 +0200

    formatting to suit linter guidelines

--- script.js
+++ script.js
@@ -3,42 +3,40 @@ const midpart = document.querySelector('.mid-part');
 const navbarLinks = document.getElementsByClassName('ul-div')[0];
 const text = document.getElementsByClassName('name')[0];
 const x = document.querySelector('.cancel-img');
-// const navitems = document.querySelectorAll('.navi-item');
 const overlay = document.getElementById('overlay');
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.add('active');
-  text.style.display = 'none';
-  toggleButton.style.display = 'none';
-  x.style.display = 'block';
-
+    navbarLinks.classList.add('active');
+    text.style.display = 'none';
+    toggleButton.style.display = 'none';
+    x.style.display = 'block';
 });
 
 function closer() {
-  x.style.display = 'none';
-  navbarLinks.classList.remove('active');
-  text.style.display = 'inline';
-  toggleButton.style.display = 'block';
+    x.style.display = 'none';
+    navbarLinks.classList.remove('active');
+    text.style.display = 'inline';
+    toggleButton.style.display = 'block';
 }
 x.addEventListener('click', closer);
 
 const popup = [
-{
-  namepop: 'Profesional Art Printing Data',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  smalldescrip: 'A daily selection of privately personalized reads; no accounts or sign-ups required has been the industrys standard',
-  carddecsrip: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-  image: ['/Images/snapshot.png'],
-  worksimages: ['/Images/grid-template.png'],
-  technologies: ['html', 'css','ruby on rails','bootstrap', 'ruby'],
-  live: ['https://tafaramafemba.github.io/The-portfolio/'],
-  source: ['https://github.com/tafaramafemba/The-portfolio'],
-}
+    {
+        namepop: 'Profesional Art Printing Data',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+        smalldescrip: 'A daily selection of privately personalized reads; no accounts or sign-ups required has been the industrys standard',
+        carddecsrip: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+        image: ['/Images/snapshot.png'],
+        worksimages: ['/Images/grid-template.png'],
+        technologies: ['html', 'css', 'ruby on rails', 'bootstrap', 'ruby'],
+        live: ['https://tafaramafemba.github.io/The-portfolio/'],
+        source: ['https://github.com/tafaramafemba/The-portfolio'],
+    }
 ];
 
 const container = document.querySelector('.grid-container');
-for(let i=0; i<6; i += 1){
-  container.innerHTML = `
+for (let i = 0; i < 6; i += 1) {
+    container.innerHTML = `
   <div class="multi">
   <img src="Images/balay.png" alt="placeholder" class="balay">
   <div class="stories">
@@ -115,15 +113,15 @@ for(let i=0; i<6; i += 1){
                     <button class="see pro">See Project</button>
                 </div>
             </div>`
-  ;
+        ;
 }
 
 //popupWindow
 let modalContainer;
 function popupWindow() {
-  modalContainer = document.createElement('section');
-  modalContainer.className = 'modalContainer';
-  modalContainer.innerHTML = `
+    modalContainer = document.createElement('section');
+    modalContainer.className = 'modalContainer';
+    modalContainer.innerHTML = `
   <div class="popup-window">
   <div class="multiflex">
   <p class="x">&#x2716;</p>
@@ -151,49 +149,48 @@ function popupWindow() {
     
 
 </div>`
-;
-midpart.appendChild(modalContainer);
+        ;
+    midpart.appendChild(modalContainer);
 }
 popupWindow();
 
-    const openModalButtons = document.getElementsByClassName('probtn')[0];
-    const closeModalButtons = document.getElementsByClassName('x')[0];
-    const popupwind = document.getElementsByClassName('modalContainer')[0];
-    
-    openModalButtons.addEventListener('click', () => {
+const openModalButtons = document.getElementsByClassName('probtn')[0];
+const closeModalButtons = document.getElementsByClassName('x')[0];
+const popupwind = document.getElementsByClassName('modalContainer')[0];
+
+openModalButtons.addEventListener('click', () => {
+    overlay.classList.add('opened');
+    popupwind.classList.add('opened');
+});
+
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('opened');
+    popupwind.classList.remove('opened');
+})
+
+closeModalButtons.addEventListener('click', () => {
+    overlay.classList.remove('opened');
+    popupwind.classList.remove('opened');
+})
+
+for (let i = 0; i < 6; i++) {
+    const openModalButtons2 = document.getElementsByClassName('see')[i];
+    const closeModalButtons2 = document.getElementsByClassName('x')[0];
+
+    openModalButtons2.addEventListener('click', () => {
         overlay.classList.add('opened');
         popupwind.classList.add('opened');
     });
-    
+
     overlay.addEventListener('click', () => {
         overlay.classList.remove('opened');
         popupwind.classList.remove('opened');
     })
-    
-    closeModalButtons.addEventListener('click', () => {
+
+    closeModalButtons2.addEventListener('click', () => {
         overlay.classList.remove('opened');
         popupwind.classList.remove('opened');
     })
+}
 
-    for(let i=0; i<6; i++){
-        const openModalButtons2 = document.getElementsByClassName('see')[i];
-        const closeModalButtons2 = document.getElementsByClassName('x')[0];
-        
-        openModalButtons2.addEventListener('click', () => {
-            overlay.classList.add('opened');
-            popupwind.classList.add('opened');
-        });
-        
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove('opened');
-            popupwind.classList.remove('opened');
-        })
-        
-        closeModalButtons2.addEventListener('click', () => {
-            overlay.classList.remove('opened');
-            popupwind.classList.remove('opened');
-        })
-    }
-    
 
-    
